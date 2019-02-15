47ba43d02cf97da73a3c9bfb46f72ae5497a0ed4
Minyus <yusuke.minami86@gmail.com>
2019-02-15 08:49:01 +0800

    added docstrings

--- pyminyus/print_progress.py
+++ pyminyus/print_progress.py
@@ -1,18 +1,28 @@
-# -*- coding: utf-8 -*-
-
 import sys
 import time
 
+
 def print_progress(iteration, total_iterations, update_interval_sec = 1.0):
+    """
+    Show progress of loop iteration
+    :param iteration: 0 for first iteration, 1 for next, and so on.
+    :type iteration: int
+    :param total_iterations: how many times the loop will iterate
+    :type total_iterations: int
+    :param update_interval_sec: how frequently the progress is updated
+    :type update_interval_sec: float
+    :return None
+    :rtype None
+    """
     global _time_started, _time_updated
-    
+
     t = time.time()
     i = iteration
     if i==0:
         _time_started = _time_updated = t
     elif ((t - _time_updated) > update_interval_sec) or i == (total_iterations - 1) :
         _time_updated = t
-        t_elapsed = t - _time_started    
+        t_elapsed = t - _time_started
         i_ = i + 1
         progress = i_ / total_iterations
         progress_pct = 100 * progress
@@ -27,8 +37,8 @@ def print_progress(iteration, total_iterations, update_interval_sec = 1.0):
         sys.stdout.flush()
     if i == (total_iterations - 1):
         print()
-        
-### Example ###
+
+
 if __name__ == "__main__":
     total_iterations = 100
     for i in range(total_iterations):
---
+++ pyminyus/product_dict_list.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+
+def product_dict_list(d):
+    """
+    Generate all the combinations of dictionary values.
+    :param d:
+    :type d: dict[str, list[any]]
+    :return list of combinations of values
+    :rtype list[dict[str, any]]
+    """
+    return list(dict(zip(d.keys(), values)) for values in product(*d.values()))
+
+
+if __name__ == "__main__":
+    params_dict = {'filters': [32, 64], 'kernel_size': [3, 4], 'activation': ['relu', 'linear']}
+    print('input:', params_dict, '\n')
+    params_list = product_dict_list(params_dict)
+    print('output:')
+    for p in params_list:
+        print(p)
