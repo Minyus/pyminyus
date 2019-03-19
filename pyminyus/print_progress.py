774e4cbb20fac5cb80a7327e5d947e5326f0c4d8
Minyus <yusuke.minami86@gmail.com>
2019-03-19 21:16:16 +0800

    format sec

--- pyminyus/print_progress.py
+++ pyminyus/print_progress.py
@@ -1,6 +1,15 @@
 import sys
 import time
 
+def format_sec(s):
+    m = s/60
+    h = m/60
+    d = h/24
+    if d >= 1: return '{:7.2f} days'.format(d)
+    if h >= 1: return '{:7.2f} hours'.format(h)
+    if m >= 1: return '{:7.2f} minutes'.format(m)
+    return '{:7.2f} seconds'.format(s)
+
 class PrintProgress():
     def __init__(self, total_iterations, overwrite=False, update_interval_sec=1.0):
         self.total_iterations = total_iterations
@@ -12,19 +21,20 @@ class PrintProgress():
 
     def print_progress(self, i_):
         t = time.time()
-        if ((t - self._time_updated) > self.update_interval_sec) or (i == self.last_iteration):
+        if ((t - self._time_updated) > self.update_interval_sec) or (i in [0, self.last_iteration]):
             self._time_updated = t
             t_elapsed = t - self._time_started
+            i_ = i + 1
             progress = i_ / self.total_iterations
             progress_pct = 100 * progress
             t_est_total = t_elapsed / progress
             t_est_remained = t_est_total - t_elapsed
             print_str = \
-                ( ' | Completed :{:d}/{:d} = {:5.2f} %'.format(i_, total_iterations, progress_pct) \
-                + ' | Elapsed: {:6.0f} sec'.format(t_elapsed) \
-                + ' | Est total: {:6.0f} sec'.format(t_est_total) \
-                + ' | Est remained: {:6.0f} sec |'.format(t_est_remained) \
-                )
+                 ' | Completed :{:d}/{:d} = {:5.2f} %'.format(i_, total_iterations, progress_pct) \
+                + ' | Elapsed: ' + format_sec(t_elapsed) \
+                + ' | Est total: ' + format_sec(t_est_total) \
+                + ' | Est remained: ' + format_sec(t_est_remained) + '|'
+                
             if self.overwrite:
                 sys.stdout.write(print_str)
                 sys.stdout.flush()
@@ -33,8 +43,8 @@ class PrintProgress():
         if (i == self.last_iteration): print()
 
 if __name__ == "__main__":
-    total_iterations = 20
+    total_iterations = 10
     pp = PrintProgress(total_iterations = total_iterations)   
-    for i in range(total_iterations):
-        pp.print_progress(i)
+    for i in range(total_iterations):     
         time.sleep(1)
+        pp.print_progress(i)
